363a5c6b321be658881fc9b8ad389d17a40d1a71
Mats Grimsgaard <mats.grimsgaard@vofo.no>
2022-06-23 10:37:10 +0200

    fix: ts errors

--- lib/sanity.ts
+++ lib/sanity.ts
@@ -4,8 +4,10 @@ import {
 } from "next-sanity";
 import createImageUrlBuilder from "@sanity/image-url";
 import { config } from "./config";
+import { SanityImageSource } from "@sanity/image-url/lib/types/types";
 
-export const urlFor = (source) => createImageUrlBuilder(config).image(source);
+export const urlFor = (source: SanityImageSource) =>
+  createImageUrlBuilder(config).image(source);
 
 export const usePreviewSubscription = createPreviewSubscriptionHook(config);
 
