ab769ecf4a081ebed164a412311fd2bc96c3dcda
Mats Grimsgaard <mats.grimsgaard@vofo.no>
2022-06-23 10:36:30 +0200

    refactor: merge date funcs

--- lib/formatTime.ts
+++ lib/formatTime.ts
@@ -2,7 +2,7 @@ import { utcToZonedTime, format } from "date-fns-tz";
 import { nb as locale } from "date-fns/locale";
 
 const timeZone = "Europe/Oslo";
-type RawTime = Date | string | number;
+export type RawTime = Date | string | number;
 
 export default function formatTime(time: RawTime, formatString: string) {
   if (!time) return null;
--- lib/shortDate.ts
+++ lib/shortDate.ts
@@ -1,12 +1,5 @@
-import { utcToZonedTime, format } from "date-fns-tz";
-import { nb as locale } from "date-fns/locale";
-
-const timeZone = "Europe/Oslo";
-type RawTime = Date | string | number;
+import formatTime, { RawTime } from "./formatTime";
 
 export default function shortDate(time: RawTime) {
-  if (!time) return null;
-  const zoned = utcToZonedTime(time, timeZone);
-
-  return format(zoned, "P", { timeZone, locale });
+  return formatTime(time, "P");
 }
