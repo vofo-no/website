4a6ca3e5b4a98c60f6f6a532b6ab3ddc103a93ee
Mats Grimsgaard <mats.grimsgaard@vofo.no>
2023-10-12 12:11:28 +0200

    refactor: move tailwind custom theme to config

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,6 +27,55 @@ module.exports = {
         "open-sans": ["var(--font-open-sans)"],
         roboto: "var(--font-roboto)",
       },
+      typography: ({ theme }) => ({
+        gray: {
+          css: {
+            "--tw-prose-links": theme("colors.blue.700"),
+            "--tw-prose-links-hover": theme("colors.crimson.500"),
+            "--tw-prose-headings-h1": theme("colors.crimson.500"),
+          },
+        },
+        invert: {
+          css: {
+            "--tw-prose-links": theme("colors.blue.200"),
+            "--tw-prose-links-hover": theme("colors.yellow.500"),
+            "--tw-prose-headings-h1": theme("colors.crimson.200"),
+          },
+        },
+        DEFAULT: {
+          css: {
+            a: {
+              "&:hover": {
+                color: "var(--tw-prose-links-hover)",
+              },
+            },
+            h1: {
+              color: "var(--tw-prose-headings-h1)",
+              fontFamily: "var(--font-roboto)",
+              fontWeight: "500",
+              marginBottom: theme("spacing.3"),
+              letterSpacing: "-0.02em",
+              lineHeight: 1.25,
+            },
+            h2: {
+              fontFamily: "var(--font-roboto)",
+              fontWeight: "500",
+              marginBottom: theme("spacing.3"),
+              marginTop: theme("spacing.10"),
+              letterSpacing: "-0.02em",
+              lineHeight: 1.25,
+            },
+            h3: {
+              fontFamily: "var(--font-roboto)",
+              fontWeight: "500",
+              marginBottom: theme("spacing.2"),
+              marginTop: theme("spacing.6"),
+              letterSpacing: "-0.01em",
+              lineHeight: 1.25,
+            },
+          },
+        },
+      }),
     },
   },
   plugins: [require("@tailwindcss/typography")],
