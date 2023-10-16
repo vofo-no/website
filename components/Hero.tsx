16368af9dca9e6c57b4276d379dac0db6506c541
Mats Grimsgaard <mats.grimsgaard@vofo.no>
2023-10-16 13:30:35 +0200

    fix: add priority to hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -39,6 +39,7 @@ export default function Hero({ banner }: Pick<HomePagePayload, "banner">) {
             fill
             className="object-cover"
             blurDataURL={blurImageUrl}
+            priority
           />
         )}
         {(title || hasLink) && (
