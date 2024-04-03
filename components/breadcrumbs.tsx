dd0fd9fd20bdbadc383ab7452a3ec86767c621e1
Mats Grimsgaard <mats.grimsgaard@vofo.no>
2024-04-03 08:38:27 +0200

    feat: use breadcrumb ui

--- components/breadcrumbs.tsx
+++ components/breadcrumbs.tsx
@@ -1,8 +1,16 @@
 "use client";
 
+import React from "react";
 import Link from "next/link";
 import { useSelectedLayoutSegments } from "next/navigation";
-import { ChevronRightIcon } from "lucide-react";
+
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
 
 const vofoRegex = /\b(vofo)\b/g;
 
@@ -11,33 +19,34 @@ function displayPathName(raw: string) {
   return str.replace("-", " ").replace(vofoRegex, "Vofo");
 }
 
-function Sep() {
-  return <ChevronRightIcon size={14} className="text-muted-foreground/30" />;
-}
-
 export function Breadcrumbs() {
   const segments = useSelectedLayoutSegments().slice(0, -1);
 
   if (!segments.length) return null;
 
   return (
-    <div className="container relative mt-2 text-sm">
-      <ol className="flex flex-wrap gap-1">
+    <Breadcrumb className="container mt-2">
+      <BreadcrumbList>
+        <BreadcrumbItem>
+          <BreadcrumbLink asChild>
+            <Link href="/">Hjem</Link>
+          </BreadcrumbLink>
+        </BreadcrumbItem>
         {segments.map((link, index) => {
           const href = `/${segments.slice(0, index + 1).join("/")}`;
           return (
-            <li key={href} className="flex items-center gap-2">
-              <Link
-                href={href}
-                className="hover:underline hover:text-primary text-muted-foreground/80"
-              >
-                {displayPathName(link)}
-              </Link>
-              <Sep />
-            </li>
+            <React.Fragment key={href}>
+              <BreadcrumbSeparator />
+              <BreadcrumbItem>
+                <BreadcrumbLink asChild>
+                  <Link href={href}>{displayPathName(link)}</Link>
+                </BreadcrumbLink>
+              </BreadcrumbItem>
+            </React.Fragment>
           );
         })}
-      </ol>
-    </div>
+        <BreadcrumbSeparator />
+      </BreadcrumbList>
+    </Breadcrumb>
   );
 }
