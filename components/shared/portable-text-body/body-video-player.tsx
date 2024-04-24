f84dfac412744a778ce6a044d93fc5c0eb63eb05
Mats Grimsgaard <mats.grimsgaard@vofo.no>
2024-04-24 10:37:18 +0200

    fix: make inline video responsive

--- components/shared/portable-text-body/body-video-player.tsx
+++ components/shared/portable-text-body/body-video-player.tsx
@@ -5,5 +5,9 @@ import dynamic from "next/dynamic";
 const ReactPlayer = dynamic(() => import("react-player/lazy"), { ssr: false });
 
 export const BodyVideoPlayer = (props: { url: string }) => {
-  return <ReactPlayer {...props} />;
+  return (
+    <div className="max-w-full aspect-video">
+      <ReactPlayer {...props} width="100%" height="100%" />
+    </div>
+  );
 };
