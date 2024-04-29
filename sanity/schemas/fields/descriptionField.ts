a08188d96a1d95927018849e36327c1666291017
Mats Grimsgaard <mats.grimsgaard@vofo.no>
2024-04-29 10:01:03 +0200

    feat: promote seo practices in sanity

--- sanity/schemas/fields/descriptionField.ts
+++ sanity/schemas/fields/descriptionField.ts
@@ -2,9 +2,17 @@ import { defineField } from "sanity";
 
 export default defineField({
   name: "description",
-  description: "En kort beskrivelse",
+  description:
+    "En kort beskrivelse. Beskrivelsen bør være unik for dette elementet.",
   title: "Beskrivelse",
   type: "text",
-  validation: (rule) => rule.max(300),
+  validation: (rule) => [
+    rule.max(300),
+    rule
+      .max(160)
+      .warning(
+        "Beskrivelsen bør ikke være for lang. Kan du skrive litt kortere?",
+      ),
+  ],
   rows: 3,
 });
