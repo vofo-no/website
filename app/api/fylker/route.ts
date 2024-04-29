640a3e4331a2c1a9dc9edac5a16723bb487976f2
Mats Grimsgaard <mats.grimsgaard@vofo.no>
2024-04-29 14:40:45 +0200

    fix: use new loader

--- app/api/fylker/route.ts
+++ app/api/fylker/route.ts
@@ -1,7 +1,7 @@
 import { loadAllCounties } from "@/sanity/loader/loadQuery";
 
 export async function GET() {
-  const { data } = await loadAllCounties();
+  const data = await loadAllCounties();
 
   return Response.json({ data });
 }
--- app/api/tema/route.ts
+++ app/api/tema/route.ts
@@ -1,7 +1,7 @@
 import { loadAllTopics } from "@/sanity/loader/loadQuery";
 
 export async function GET() {
-  const { data } = await loadAllTopics();
+  const data = await loadAllTopics();
 
   return Response.json({ data });
 }
