5b018ab45ecb1ebd6eafd0ae21b9dd092774afc8
Mats Grimsgaard <mats.grimsgaard@vofo.no>
2024-05-06 14:08:41 +0200

    feat: enable vercel analytics

--- app/(www)/layout.tsx
+++ app/(www)/layout.tsx
@@ -1,5 +1,6 @@
 import { Metadata } from "next";
 import { draftMode } from "next/headers";
+import { Analytics } from "@vercel/analytics/react";
 
 import { AutomaticVisualEditing } from "@/components/automatic-visual-editing";
 import { Breadcrumbs } from "@/components/breadcrumbs";
@@ -23,6 +24,7 @@ export default function WwwLayout({ children }: { children: React.ReactNode }) {
         <SiteFooter />
       </div>
       {draftMode().isEnabled && <AutomaticVisualEditing />}
+      <Analytics />
     </>
   );
 }
