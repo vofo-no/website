e9b39fa268a7af372ce49e890b0f51bf8067c1ea
Mats Grimsgaard <mats.grimsgaard@vofo.no>
2024-05-15 11:49:53 +0200

    feat: add eco lighthouse

--- components/footer-sponsor.tsx
+++ components/footer-sponsor.tsx
@@ -1,10 +1,22 @@
 import Image from "next/image";
 
+import Miljofyrtarn from "./miljofyrtarn.png";
 import PoweredByVercel from "./powered-by-vercel.svg";
 
 export function FooterSponsor() {
   return (
     <div className="flex flex-wrap justify-center items-center pt-8 gap-x-12 gap-y-4 text-sm">
+      <a
+        href="/tema/baerekraft"
+        title="Vofo er Miljøfyrtårn-sertifisert. Det betyr at vi tar bærekraft på alvor og har oppfylt strenge miljøkrav."
+      >
+        <Image
+          src={Miljofyrtarn}
+          alt="Miljøfyrtårn-sertifisert virksomhet"
+          className="max-w-full"
+          height={75}
+        />
+      </a>
       <a
         href="https://vercel.com/?utm_source=vofo-kursinfo&utm_campaign=oss"
         target="_blank"
