aa27c2109bfc3873aec6726e9bdf3fd24764b247
Mats Grimsgaard <mats.grimsgaard@vofo.no>
2024-05-27 12:53:16 +0200

    feat: support full range of react-player sources

--- components/shared/portable-text-body/body-video-player.tsx
+++ components/shared/portable-text-body/body-video-player.tsx
@@ -7,7 +7,7 @@ const ReactPlayer = dynamic(() => import("react-player/lazy"), { ssr: false });
 export const BodyVideoPlayer = (props: { url: string }) => {
   return (
     <div className="max-w-full aspect-video">
-      <ReactPlayer {...props} width="100%" height="100%" />
+      <ReactPlayer {...props} controls width="100%" height="100%" />
     </div>
   );
 };
--- sanity/schemas/objects/youtube-preview.tsx
+++ sanity/schemas/objects/youtube-preview.tsx
@@ -1,5 +1,5 @@
 import { Flex, Text } from "@sanity/ui";
-import YouTubePlayer from "react-player/youtube";
+import ReactPlayer from "react-player/lazy";
 import type { PreviewProps } from "sanity";
 
 export function YouTubePreview(props: PreviewProps) {
@@ -8,7 +8,7 @@ export function YouTubePreview(props: PreviewProps) {
   return (
     <Flex padding={3} align="center" justify="center">
       {typeof url === "string" ? (
-        <YouTubePlayer url={url} />
+        <ReactPlayer url={url} controls />
       ) : (
         <Text>Legg inn en YouTube-URL</Text>
       )}
