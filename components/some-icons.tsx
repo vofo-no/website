a0e440fdedef4b43cd3135737a770298fe57caaa
Mats Grimsgaard <mats.grimsgaard@vofo.no>
2024-11-04 11:28:36 +0100

    fix: use recommended brand icons

--- components/some-icons.tsx
+++ components/some-icons.tsx
@@ -1,6 +1,10 @@
-import { FacebookIcon, LucideIcon, YoutubeIcon } from "lucide-react";
+import {
+  IconType,
+  SiFacebook,
+  SiYoutube,
+} from "@icons-pack/react-simple-icons";
 
-export const SomeIcons: Record<string, LucideIcon> = {
-  Facebook: FacebookIcon,
-  YouTube: YoutubeIcon,
+export const SomeIcons: Record<string, IconType> = {
+  Facebook: SiFacebook,
+  YouTube: SiYoutube,
 };
